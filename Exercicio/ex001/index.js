25afc8b4b58f39ce28a3c255d044299be9a907d0
Bruno Pontes <145600998+brunopontesf38@users.noreply.github.com>
2024-06-17 21:59:49 -0300

    texte de js

---
+++ Exercicio/ex001/index.js
@@ -0,0 +1,6 @@
+ /*let nome = window.prompt('Escreva teu nome: '); 
+ let idade = window.prompt('Qual sua idade? ');
+ alert(`Olá! ${nome} sua idade é ${idade}`);
+ alert(`Sinto muito ${nome} você não me passa muita confiança!`)
+ alert(`${nome} só para deixar claro não é nada pessoal! bjs querida.`)*/
+
