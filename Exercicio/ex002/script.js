1802d1dd23eba487ac7433f725483d5af60ff863
Bruno Pontes <145600998+brunopontesf38@users.noreply.github.com>
2024-07-08 20:26:24 -0300

    001

---
+++ Exercicio/ex002/script.js
@@ -0,0 +1,9 @@
+const nome = window.prompt('Qual é o seu nome? ');
+window.alert(`prazer em conhecer ${nome}!`);
+
+let n1 = Number(window.prompt('Escreva um numero: ')); 
+let n2 = Number(window.prompt('Escreva outro numero: '));
+
+const s = n1 + n2 ;
+
+window.alert(`A soma entre ${n1} e ${n2} é igual ${s}`);
---
+++ Exercicio/ex003/script.js
@@ -0,0 +1,10 @@
+let nome = window.prompt('Escreva seu nome: ')
+alert(`Olá! ${nome}, vamor comerçar!`)
+
+var corpo = window.document.body
+var p1 = window.document.getElementsByTagName('p')[2]
+window.document.write(' Com menas preocupação ' + p1.innerText);
+
+
+corpo.style.backgroundColor = 'pink';
+ 
